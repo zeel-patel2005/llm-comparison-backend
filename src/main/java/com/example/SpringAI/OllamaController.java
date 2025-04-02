224c2514a345381755ae6663ef9e18d636c33e11
zeel patel <zeelp4797@gmail.com>
2025-04-02 19:14:40 +0530

    using ollama ai model is deepseek

--- src/main/java/com/example/SpringAI/OllamaController.java
+++ src/main/java/com/example/SpringAI/OllamaController.java
@@ -1,25 +1,30 @@
 package com.example.SpringAI;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/openai")
+@RequestMapping("/api/ollama")
 @CrossOrigin
-public class OpenAIController {
-
+public class OllamaController {
 
     private ChatClient chatModel;
 
-    public OpenAIController(OpenAiChatModel openAiChatModel){
+    public OllamaController(OllamaChatModel openAiChatModel){
         this.chatModel= ChatClient.create(openAiChatModel);
     }
 
     @GetMapping("/{message}")
     public ResponseEntity<String> getAnswer(@PathVariable String message){
-        String response = chatModel.prompt(message).call().content();
+        ChatResponse chatResponse = chatModel.prompt(message).call().chatResponse();
+
+        String response = chatResponse.getResult().getOutput().getText();
         return ResponseEntity.ok(response);
     }
 }
+
+
